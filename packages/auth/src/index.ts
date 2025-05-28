e5a3706bdeee9f9cc70e012936cab49d9b6418cb
Thiago Cabral <thiagocabral477@gmail.com>
2025-05-28 13:39:48 -0300

    feat: add permissions structure

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,3 +1,44 @@
+import {
+  AbilityBuilder,
+  type CreateAbility,
+  createMongoAbility,
+  type MongoAbility
+} from "@casl/ability"
+import z from "zod"
+import type { User } from "./models/user"
+import { permissions } from "./permissions"
+import { billingSubject } from "./subjects/billing"
+import { inviteSubject } from "./subjects/invite"
+import { organizationSubject } from "./subjects/organization"
+import { projectSubject } from "./subjects/project"
+import { userSubject } from "./subjects/user"
 
+const appAbilitiesSchema = z.union([
+  userSubject,
+  projectSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([z.literal("manage"), z.literal("all")])
+])
 
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== "function") {
+    throw new Error(`Permission for role ${user.role} not found`)
+  }
+
+  permissions[user.role](user, builder)
+  const ability = builder.build({
+    detectSubjectType: (subject) => {
+      return subject.__typename
+    }
+  })
+  return ability
+}
